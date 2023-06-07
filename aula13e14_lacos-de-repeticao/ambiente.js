d29b017d6d0915ce48ed8862eb5f2c9259bb2bfa
Lucas Santos <lucasarosss@gmail.com>
2023-06-06 23:15:17 -0300

    finalizei aulas 13 e 14

---
+++ aula13e14_lacos-de-repeticao/ambiente.js
@@ -0,0 +1,16 @@
+// let i = 1
+
+// while (i <= 4 ) {
+//     console.log("Tudo Bem!")
+//     i++
+// }
+
+
+
+let i = 1
+
+do {
+    console.log("Tudo Bem!" + i)
+    i++
+} while (i <= 4 ) 
+
---
+++ aula13e14_lacos-de-repeticao/laco-for.js
@@ -0,0 +1,5 @@
+//FOR
+//for (inicio; teste; incremento))
+for (let i = 1; i <= 21; i++) {
+    console.log(`Número ${i}`)
+  }
